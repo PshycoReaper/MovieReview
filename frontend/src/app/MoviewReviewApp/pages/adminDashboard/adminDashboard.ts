a88ae23359d6692b66e41ab57fe09e6ffdddbafb
PshycoReaper <164075899+PshycoReaper@users.noreply.github.com>
2026-06-30 11:06:06 -0600

    fix: dashboard style

--- frontend/src/app/MoviewReviewApp/pages/adminDashboard/adminDashboard.ts
+++ frontend/src/app/MoviewReviewApp/pages/adminDashboard/adminDashboard.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+import { CommonModule} from '@angular/common';
 
+register();
 @Component({
   selector: 'adminDashboard',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './adminDashboard.html',
-  styleUrl:'./adminDashboardStyle.css'
 })
 export class AdminDashboard {
+  imageUrl = 'https://image.tmdb.org/t/p/w500';
 
+  movies: any[] = [];
 }
