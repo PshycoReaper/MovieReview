543f3ef14ebc130de7533e8edb9682ce5a14542f
Aurora Martinez <al222397@alumnos.uacj.mx>
2026-07-09 21:57:25 -0600

    feat: Searchbar done

--- frontend/src/app/MoviewReviewApp/pages/MovieSearchPage/MovieSearchPage.ts
+++ frontend/src/app/MoviewReviewApp/pages/MovieSearchPage/MovieSearchPage.ts
@@ -2,10 +2,13 @@ import { CommonModule } from "@angular/common";
 import { Component, inject, signal } from "@angular/core";
 import { DBconexion } from "../../services/DataBase/dbconexion";
 import { Movie } from "../../Interfaces/movie.interface";
+import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormContactRequest } from "../../shared/FormContactRequest/FormContactRequest";
+import { of, switchMap } from "rxjs";
 
 @Component({
     selector: 'app-movie-search',
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule],
     templateUrl: './MovieSearchPage.html',
 })
 
@@ -14,9 +17,39 @@ export class MovieSearchPage {
 
     moviesDBMongoArray = signal<Movie[]>([]);
 
+    buscarControlSearch = new FormControl('');
+    resultadosPelisBusqueda = signal<any[]>([]);
+
+    //PARA TRAER TODAS LAS PELÍCULAS DE MONGODB
     constructor() {
         this.dbconexion.getMovies().subscribe((movies: Movie[]) => {
             this.moviesDBMongoArray.set(movies)
         })
     }
+
+    //PARA LA BARRA DE BÚSQUEDA
+      ngOnInit() {
+        this.buscarControlSearch.valueChanges
+          .pipe(
+            switchMap((nombre) => {
+              if (!nombre || nombre.trim().length < 2) {
+                this.resultadosPelisBusqueda.set([]);
+                return of([]);
+              }
+    
+              return this.dbconexion.getMovieByName(nombre);
+            }),
+          )
+          .subscribe({
+            next: (pelis) => {
+              this.resultadosPelisBusqueda.set(pelis || []);
+            },
+    
+            error: () => {
+              this.resultadosPelisBusqueda.set([]);
+            },
+          });
+      }
+
+
 }
